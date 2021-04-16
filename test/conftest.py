4134b267e96390367e6b4a51ea8485c2c7036174
Henrik Mettler <henrik.mettler@gmail.com>
2021-04-16 10:44:07 +0200

    Add misssing conftest.py to git

--- test/conftest.py
+++ test/conftest.py
@@ -3,5 +3,20 @@ from pytest import fixture
 
 
 @fixture
-def rng_seed():
+def seed():
     return 1234
+
+
+@fixture
+def network_params():
+    return {"n_inputs": 2, "n_hidden": 4, "n_outputs": 3, "learning_rate": 0.01, }
+
+
+@fixture
+def states():
+    return [np.array([0.1, 0.5]), np.array([0.75, 0.25])]
+
+
+@fixture
+def rewards():
+    return [0.75, 0.85]
