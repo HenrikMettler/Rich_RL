5f25b422a28551873eb2c95b984a185db5cca01a
Henrik Mettler <henrik.mettler@gmail.com>
2021-06-01 16:34:20 +0200

    Added framework for loading params from pickled file (for cscs jobs)

--- main.py
+++ main.py
@@ -3,47 +3,87 @@ import gym
 import torch
 import sympy
 import functools
-import warnings
 import time
-import cProfile
+import pickle
 
-from typing import Optional
+from typing import Optional, AnyStr
 
 import cgp
 
 from network import Network
-from functions import update_el_traces, update_weights_online_with_rule
-from variables import onlinelr_dna_20internal
+from functions import update_el_traces, update_weights_online_with_rule, initialize_genome_with_rxet_prior
 from operators import Const05Node, Const2Node
 
-gamma = 0.9
-n_episodes_hurdle = 1000
-cum_reward_threshold = 250000  # empirical value that policy gradient receives after ~ 3000 episodes
+using_pickled_params = True  # option for pre written parameters written in write_job
 
-use_online_init = True
+if using_pickled_params:
+    with open('params.pickle', 'rb') as f:
+        params = pickle.load(f)
+
+    gamma = params['gamma']
+    use_online_init = params['use_online_init']
+    seed = params['seed']
+    n_episodes = params['n_episodes']
+    cum_reward_threshold = params['cum_reward_threshold']
+    population_params = params['population_params']
+    ea_params = params['ea_params']
+    evolve_params = params['evolve_params']
+    genome_params = params['genome_params']
+
+else:
+    gamma = 0.9
+    use_online_init = True
+    seed = 12345
+    n_episodes = 100
+    cum_reward_threshold = 250000
+    n_episodes_reward_expectation = 100
+    population_params = {"n_parents": 1, "seed": seed}
+    ea_params = {"n_offsprings": 4, "mutation_rate": 0.03, "reorder_genome": True, "n_processes": 1,
+                 "hurdle_percentile": [0.5, 0.0], }
+    evolve_params = {"max_generations": 2}
+
+    genome_params = {
+        "n_inputs": 4,  # reward, el_traces, done (episode termination), expected_cum_reward_episode
+        "n_outputs": 1,
+        "n_columns": 300,
+        "n_rows": 1,
+        "levels_back": None,
+        "primitives": (cgp.Mul, cgp.Add, cgp.Sub, cgp.ConstantFloat, Const05Node, Const2Node)
+    }
 
 
 def set_initial_dna(ind):
     genome = cgp.Genome(**genome_params)
-    genome.dna = onlinelr_dna_20internal
+    genome.dna = initialize_genome_with_rxet_prior(n_inputs=genome_params["n_inputs"],
+                                                   n_hidden=genome_params["n_columns"],
+                                                   n_operators=len(genome_params["primitives"]),
+                                                   max_arity=2,
+                                                   rng= np.random.default_rng(seed=1234) # Todo: is using a fixed seed here an issue?
+                                                   )
+
     return cgp.IndividualSingleGenome(genome)
 
 
-def inner_objective_one(
+def inner_objective(
     t: torch.nn.Module,
     network: Network,
     env: gym.Env,
-    n_episodes: int,
     seed: int,
     rng: np.random.Generator,
+    mode: AnyStr,
+    gamma: Optional[float] = 0.9,
     n_steps_per_run: Optional[int] = 200,
+    n_episodes: Optional[int] = 0,
+    cum_reward_threshold: Optional[int] = 0,
+    n_episodes_reward_expectation: Optional[float] = 100,
 
 ) -> float:
 
     env.seed(seed)
     cum_reward = 0
     episode_counter = 0
-    for _ in range(n_episodes):
+    expected_cum_reward_per_episode = 0
+    while episode_counter < n_episodes or cum_reward <= cum_reward_threshold:
         state = env.reset()
         el_traces = torch.zeros([network.output_layer.out_features, network.output_layer.in_features + 1])
         discounted_reward = 0
@@ -62,20 +102,32 @@ def inner_objective_one(
             el_traces = update_el_traces(el_traces, probs, hidden_activities, action)
 
             update_weights_online_with_rule(rule=t, network=network, reward=reward, el_traces=el_traces,
-                                  log_prob=log_prob, discounted_reward=discounted_reward, done= done)
+                                  log_prob=log_prob, discounted_reward=discounted_reward, done= done,
+                                            expected_cum_reward_per_episode=expected_cum_reward_per_episode)
 
             if done:
                 episode_counter += 1
                 break
             state = new_state
 
+        # todo: "document" variable
+        expected_cum_reward_per_episode = (1-1/n_episodes_reward_expectation)*\
+                                          expected_cum_reward_per_episode + \
+                                          (1/n_episodes_reward_expectation)*cum_reward/n_steps_per_run
     env.close()
+    if mode == 'reward_max':
+        return float(cum_reward)
+    elif mode == 'episode_min':
+        return float(-episode_counter)
+    else:
+        raise AssertionError('Mode not available')
 
-    return cum_reward
 
-
+# Todo: - cache
 def objective_one(
     individual: cgp.IndividualSingleGenome,
+    n_episodes: int,
+    gamma: float,
     seed: int,
     rng: np.random.Generator,
 ):
@@ -93,63 +145,21 @@ def objective_one(
 
     t = individual.to_torch()
     try:
-        individual.fitness = -inner_objective_one(t=t, network=network, env=env,
-                                              n_episodes=n_episodes_hurdle,
-                                              seed=seed, rng=rng)
+        individual.fitness = inner_objective(t=t, network=network, env=env,
+                                              n_episodes=n_episodes, gamma=gamma,
+                                              seed=seed, rng=rng, mode='reward_max')
     except ZeroDivisionError:
         individual.fitness = -np.inf
 
+    # Todo write network.state_dict() to ind (and possibly pickle dumps)
     individual.network = network # assign the trained network to the individual for objective 2
     return individual
 
 
-def inner_objective_two(
-    t: torch.nn.Module,
-    network: Network,
-    env: gym.Env,
-    cum_reward_threshold: int,
-    seed: int,
-    rng: np.random.Generator,
-    n_steps_per_run: Optional[int] = 200,
-
-) -> float:
-
-    env.seed(seed)
-    cum_reward = 0
-    episode_counter = 0
-    while cum_reward < cum_reward_threshold:
-        state = env.reset()
-        el_traces = torch.zeros([network.output_layer.out_features, network.output_layer.in_features + 1])
-        discounted_reward = 0
-
-        for _ in range(n_steps_per_run):
-            action, probs, hidden_activities = network.get_action(state, rng)
-
-            hidden_activities = torch.cat((hidden_activities, torch.ones(1)), 0)
-            log_prob = torch.log(probs.squeeze(0)[action])
-
-            new_state, reward, done, _ = env.step(action)
-            discounted_reward *= gamma
-            discounted_reward += reward
-            cum_reward += reward
-
-            el_traces = update_el_traces(el_traces, probs, hidden_activities, action)
-
-            update_weights_online_with_rule(rule=t, network=network, reward=reward, el_traces=el_traces,
-                                  log_prob=log_prob, discounted_reward=discounted_reward, done= done)
-
-            if done:
-                episode_counter += 1
-                break
-            state = new_state
-
-    env.close()
-
-    return float(episode_counter)
-
-
 def objective_two(
     individual: cgp.IndividualSingleGenome,
+    cum_reward_threshold: int,
+    gamma: float,
     seed: int,
     rng: np.random.Generator,
 ):
@@ -161,62 +171,50 @@ def objective_two(
 
     t = individual.to_torch()
     try:
-        individual.fitness = -inner_objective_two(t=t, network=individual.network, env=env,
-                                              cum_reward_threshold=cum_reward_threshold,
-                                              seed=seed, rng=rng)
+        individual.fitness = inner_objective(t=t, network=individual.network, env=env,
+                                             cum_reward_threshold=cum_reward_threshold,
+                                             gamma=gamma, seed=seed, rng=rng, mode='episode_min')
     except ZeroDivisionError:
         individual.fitness = -np.inf
 
     return individual
 
 
-seed = 1234
 rng = np.random.default_rng(seed=seed)
 
-# population and evolutionary algorithm initialization
-population_params = {"n_parents": 1, "seed": seed}
-genome_params = {
-    "n_inputs": 3,  # reward, el_traces, done (episode termination)
-    "n_outputs": 1,
-    "n_columns": 20,
-    "n_rows": 1,
-    "levels_back": None,
-    "primitives": (cgp.Mul, cgp.Add, cgp.Sub, cgp.ConstantFloat, Const05Node, Const2Node) #(cgp.Add, cgp.Sub, cgp.Mul, cgp.ConstantFloat),
-}
-ea_params = {"n_offsprings": 4, "mutation_rate": 0.03, "reorder_genome": True, "n_processes": 1,
-             "hurdle_percentile": [0.5, 0.0],}
-evolve_params = {"max_generations": 100, "termination_fitness": -1000}  # Todo: set reasonable termination fitness
-
 if use_online_init:
-    pop = cgp.Population(**population_params, genome_params=genome_params, individual_init=set_initial_dna)
+    pop = cgp.Population(**population_params, genome_params=genome_params,
+                         individual_init=set_initial_dna)
 else:
     pop = cgp.Population(**population_params, genome_params=genome_params)
 
 ea = cgp.ea.MuPlusLambda(**ea_params)
 
-# initialize a history
 history = {}
 history["fitness_champion"] = []
-# history["expression_champion"] = []
+history["expression_champion"] = []
 
 
 def recording_callback(pop):
     history["fitness_champion"].append(pop.champion.fitness)
-    # history["expression_champion"].append(pop.champion.to_sympy())
+    history["expression_champion"].append(pop.champion.to_sympy())
 
 
 obj_1 = functools.partial(
     objective_one,
+    n_episodes=n_episodes,
+    gamma=gamma,
     seed=seed,
     rng=rng
 )
 
 obj_2 = functools.partial(
     objective_two,
+    cum_reward_threshold=cum_reward_threshold,
+    gamma=gamma,
     seed=seed,
     rng=rng
 )
-
 start = time.time()
 cgp.evolve(pop, [obj_1, obj_2], ea, **evolve_params, print_progress=True, callback=recording_callback)
 end = time.time()
@@ -224,7 +222,7 @@ print(f"Time elapsed:", end - start)
 
 max_fitness = history["fitness_champion"][-1]
 best_expr = pop.champion.to_sympy()
-# best_expr = best_expr.replace("x_0", "pre").replace("x_1", "post").replace("x_2, "weight")
-# .replace("x_4", "reward")
+
 print(
     f'Learning rule with highest fitness: "{best_expr}" (fitness: {max_fitness})')
+
