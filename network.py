8c7e662e1c4c78e0240a3e2bc511a196bdfd2260
Henrik Mettler <henrik.mettler@gmail.com>
2021-10-12 16:39:31 +0200

    Add VanillaRNN class

--- network.py
+++ network.py
@@ -26,8 +26,6 @@ class Network(nn.Module):
             Number of output neurons (should be the dimension of the action space)
         learning_rate: float
             Learning rate
-        use_autograd_for_output: bool
-            Whether to use autograd for the output layer parameters. Defaults to true
         """
 
         super().__init__()
@@ -83,3 +81,67 @@ class Network(nn.Module):
             #print('probs are nan')
             selected_action = rng.choice(self.num_actions)
         return selected_action, probs, hidden_activities.squeeze()
+
+
+class VanillaRNN(nn.Module):
+    def __init__(self, n_inputs: int, n_hidden: int, n_outputs: int, learning_rate: float,
+                 use_autograd=True) \
+            -> None:
+        """ Init function
+
+        Parameters
+        ----------
+        n_inputs: int
+            Number of network inputs (should be the dimension of the observation space)
+        n_hidden: int
+            Number of hidden layer neurons
+        n_outputs: int
+            Number of output neurons (should be the dimension of the action space)
+        learning_rate: float
+            Learning rate
+        use_autograd: bool
+            Whether to use autograd parameter updates. Defaults to true
+        """
+
+        super().__init__()
+        self.num_hidden = n_hidden
+        self.num_actions = n_outputs
+
+        self.rnn = nn.RNN(n_inputs, n_hidden)
+        self.output_layer = nn.Linear(n_hidden, n_outputs)
+
+        self.learning_rate = learning_rate
+
+        if not use_autograd:
+            # turn off autograd for all parameters
+            for parameter in self.named_parameters():
+                parameter.requires_grad = False
+        else:
+            # optimizer for baseline with policy gradient
+            self.optimizer = optim.SGD(self.parameters(), lr=self.learning_rate)
+
+    def forward(self, state: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """ Compute a forward pass in the network and return output and hidden activities
+
+        Parameters
+        ----------
+        state: torch.Tensor
+            Current state input from environment
+
+        Returns
+        -------
+        hidden_activities: torch.Tensor
+            Hidden layer activities
+        output_activities: torch.Tensor
+            Output layer activities
+        """
+
+        hidden = self.init_hidden()
+
+        out, hidden = self.rnn(state, hidden)
+        output = self.output_layer(out)
+
+        return output, hidden
+
+    def init_hidden(self) -> torch.Tensor:
+        return torch.zeros(self.num_hidden)  # Todo: should these be zeros? (is so in most tutorials)
