7da8868922815b1597d0151bbd21f923f466b849
Henrik Mettler <henrik.mettler@gmail.com>
2021-10-18 16:51:37 +0200

    write job for full runs

--- minigrid_baseline.py
+++ minigrid_baseline.py
@@ -19,7 +19,7 @@ seed = 123456789
 torch.manual_seed(seed=seed)
 rng = np.random.default_rng(seed=seed)
 
-n_episodes: int =  params['n_episodes'] #50000
+n_episodes: int = params['n_episodes'] #50000
 n_steps_max: int = 1000
 env = DynamicMiniGrid(seed=seed)
 env, is_solvable = alter_env(env, n=6)
@@ -29,7 +29,7 @@ state = env.respawn()["image"].flatten()
 n_inputs: int = np.size(state)
 n_hidden: int = params['n_hidden']  # todo: iterate over different sizes
 n_outputs: int = 3  # Left, right, forward (pick up, drop, toggle, done are ingnored); env.action_space.n
-learning_rate: float =  params['n_hidden'] #1e-4  # todo iterate over different lr's
+learning_rate: float = params['learning_rate'] #1e-4  # todo iterate over different lr's
 
 policy_net = Network(n_inputs=n_inputs, n_hidden=n_hidden, n_outputs=n_outputs,
                      learning_rate=learning_rate, weight_update_mode=weight_update_mode)
