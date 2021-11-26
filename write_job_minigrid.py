b7e2f1baaedd5c52fc630a90bdfea1df2b1cc1cd
Henrik Mettler <henrik.mettler@gmail.com>
2021-11-26 18:58:00 +0100

    write_job_minigrid.py for cgp run

--- write_job_minigrid.py
+++ write_job_minigrid.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         'account': 'ich029m',
         'constraint': 'mc',
         'partition': 'normal',
-        'sim_script': 'minigrid_with_alter.py',
+        'sim_script': 'minigrid.py',
         'dependencies': ['functions.py', 'network.py', 'operators.py'],
 
         # experiment configuration
@@ -42,13 +42,13 @@ if __name__ == '__main__':
     params['md5_hash_sim_script'] = utils.md5_file(params['sim_script'])  # consistency check
     params['md5_hash_dependencies'] = [utils.md5_file(fn) for fn in params['dependencies']]  # consistency check
 
-    #results_folder = 'cgp_minigrid_run'
-    results_folder = 'comp_autograd_eq4'
+    results_folder = 'cgp_minigrid_run'
+    #results_folder = 'comp_autograd_eq4'
 
-    weight_update_modes = ['equation4', 'autograd']
-    #for use_rxet_init in [False, True]:
+    #weight_update_modes = ['equation4', 'autograd']
+    for use_rxet_init in [False, True]:
 
-    for weight_update_mode in weight_update_modes:
+    #for weight_update_mode in weight_update_modes:
         for reset in [True, False]:
 
             params['weight_update_mode'] = weight_update_mode
