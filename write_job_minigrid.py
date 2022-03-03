88a4ff1ae5c092c5a23c21886b530c8f0e317358
Henrik Mettler <henrikmettler@staff-210-103.eduroam.unibe.ch>
2022-03-03 15:06:24 +0100

    write_job for temp nov only runs

--- write_job_minigrid.py
+++ write_job_minigrid.py
@@ -58,14 +58,14 @@ if __name__ == '__main__':
 
         # cgp parameterisation
         'max_time': 82800,  # 82800s~23h
-        'genome_params': {"n_inputs": 4, },
+        'genome_params': {"n_inputs": 3, },
         'ea_params': {'n_processes': 4, },
     }
 
     params['md5_hash_sim_script'] = utils.md5_file(params['sim_script'])  # consistency check
     params['md5_hash_dependencies'] = [utils.md5_file(fn) for fn in params['dependencies']]  # consistency check
 
-    results_folder = 'temp_and_spat_nov'
+    results_folder = 'temp_nov'
 
     for use_rxet_init in [True, False]:
 
